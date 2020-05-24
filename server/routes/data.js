143dd14232d928c8f34d28d294bd1f160176b038
Michael Standen <screaminghawk@gmail.com>
2020-05-25 08:21:42 +1200

    Data shows users

--- server/routes/data.js
+++ server/routes/data.js
@@ -10,11 +10,21 @@ module.exports = (io, socket, discord) => {
 				id: c.id,
 				name: c.name,
 				type: c.type,
-			}))
+			})),
+			users: g.members.map(m => ({
+				id: m.user.id,
+				username: m.user.username,
+				bot: m.user.bot,
+			})),
 		}))
 	data.channels = data.guilds.map(g => (
 			g.channels.filter(c => c.type === "text")
 				.map(c => ({key: c.id, value: `${g.name} - ${c.name}`}))
 		)).reduce((a, b) => a.concat(b), [])
+	data.users = discord.users.filter(u => !u.bot)
+			.map(u => ({
+				key: u.id,
+				value: u.username,
+			}))
 	socket.emit('data is', data)
 }
