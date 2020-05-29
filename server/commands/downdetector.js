8eba11992b486dbd89b05177252f12285bc9ee03
Michael Standen <screaminghawk@gmail.com>
2020-05-29 21:02:40 +1200

    Enhanced down detector message

--- server/commands/downdetector.js
+++ server/commands/downdetector.js
@@ -5,18 +5,18 @@ const log = require('../util/logger')
 
 module.exports = (discord) => {
 	// Check civ server status
-	discord.setCommand(/civ.* down\??$/i, msg => {
+	discord.setCommand(/civ.* (up|down)\??$/i, msg => {
 		log.debug('Checking if Civ is down...')
 		fetch('https://downdetector.com/status/civilization')
 				.then(res => res.text())
 				.then(text => cheerio.load(text))
 				.then(page => page('.entry-title').text().indexOf('No') > -1)
 				.then(up => {
-					log.debug(`Servers are ${up ? 'up' : 'down'}`)
+					log.debug(`Down Detector says servers are ${up ? 'up' : 'down'}`)
 					if (up){
-						msg.channel.send("Civ servers are up!!! :tada:")
+						msg.channel.send('Down Detector says Civ servers are up!!! :tada:')
 					} else {
-						msg.channel.send('Confirmed. Civilization servers are still having problems... :sob:')
+						msg.channel.send('Civilization servers are still having problems... :sob:')
 					}
 				})
 				.catch(err => {
