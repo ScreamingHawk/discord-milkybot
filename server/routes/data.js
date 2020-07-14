879b111a54cdad2c809b05afe9d7982f00e6117d
Michael Standen <screaminghawk@gmail.com>
2020-07-14 12:57:41 +1200

    Update discordjs to v12

--- server/routes/data.js
+++ server/routes/data.js
@@ -9,21 +9,21 @@ module.exports = {
 		allSockets = io
 		log.debug('Sending discord data')
 		const data = {}
-		data.guilds = discord.guilds.map(g => ({
+		data.guilds = discord.guilds.cache.map(g => ({
 				id: g.id,
 				name: g.name,
-				channels: g.channels.map(c => ({
+				channels: g.channels.cache.map(c => ({
 					id: c.id,
 					name: c.name,
 					type: c.type,
 				})),
-				users: g.members.map(m => ({
+				users: g.members.cache.map(m => ({
 					id: m.user.id,
 					username: m.user.username,
 					bot: m.user.bot,
 				})),
 			}))
-		data.users = discord.users.filter(u => !u.bot)
+		data.users = discord.users.cache.filter(u => !u.bot)
 				.map(u => ({
 					key: u.id,
 					value: u.username,
